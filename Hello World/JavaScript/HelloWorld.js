f857689188a483d63c05ad98b126fe0fde701047
Ali Cherif <ga_hammas@esi.dz>
2019-10-08 01:45:53 +0200

    Hello World JS

--- Hello World/JavaScript/HelloWorld.js
+++ Hello World/JavaScript/HelloWorld.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+console.log("H");
+console.log("e");
+console.log("l");
+console.log("l");
+console.log(" ");
+console.log("W");
+console.log("o");
+console.log("r");
+console.log("l");
+console.log("d");
